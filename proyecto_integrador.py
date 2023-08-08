8f3e972f5addc3508b272a3453e6c2a0d57fc902
Neil Benavides <nbenavides07@gmailcom>
2023-08-08 09:45:50 -0500

    EL proyecto integrador parte 2 ha sido agregado.

--- proyecto_integrador.py
+++ proyecto_integrador.py
@@ -1,3 +1,21 @@
+# Proyecto Integrador Parte 1
+
 print("¡Hola! ¿Cuál es tu nombre para que podamos conocernos mejor?")
 nombre = input()
-print(f"¡Hola, hola! {nombre}, Un aplauso virtual por unirte a nosotros.")
+print(f"¡Hola, hola! {nombre}, Un aplauso virtual por unirte a nuestro juego.")
+
+
+# Proyecto Integrador Parte 2
+
+import readchar
+
+print("Presiona cualquier tecla para empezar")
+
+while True:
+    teclas = readchar.readkey()
+    print("La tecla presionada es:", teclas)
+
+    if teclas == readchar.key.UP:
+        print("El programa ha sido detenido.")
+        break
+    
