7e2e3ef612ea89d062b1f57890c287a6cfe276b9
Neil Benavides <nbenavides07@gmailcom>
2023-08-11 10:42:58 -0500

    proyecto Integrador parte 2

---
+++ proyecto_integrador2.py
@@ -0,0 +1,11 @@
+import readchar
+
+print("Presiona cualquier tecla para empezar")
+
+while True:
+    teclas = readchar.readkey()
+    print("La tecla presionada es:", teclas)
+
+    if teclas == readchar.key.UP:
+        print("El programa ha sido detenido.")
+        break
